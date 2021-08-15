fb08d56456b485e4962a6a96883b577f43126537
venice94 <venice9394@gmail.com>
2021-08-15 23:30:25 +0800

    update after review

--- projects/03_coffee_shop_full_stack/starter_code/backend/src/api.py
+++ projects/03_coffee_shop_full_stack/starter_code/backend/src/api.py
@@ -3,7 +3,6 @@ from flask import Flask, request, jsonify, abort
 from sqlalchemy import exc
 import json
 from flask_cors import CORS
-import traceback
 
 from .database.models import db_drop_and_create_all, setup_db, Drink
 from .auth.auth import AuthError, requires_auth
@@ -21,7 +20,9 @@ def get_drinks():
     if drinks is None:
         abort(404)
 
-    return jsonify({"success":True, "drinks":[drink.short() for drink in drinks]})
+    return jsonify({"success": True,
+                    "drinks": [drink.short() for drink in drinks]})
+
 
 @app.route('/drinks-detail', methods=['GET'])
 @requires_auth("get:drinks-detail")
@@ -30,7 +31,9 @@ def get_drink_details(permission):
     if drinks is None:
         abort(404)
 
-    return jsonify({"success":True, "drinks":[drink.long() for drink in drinks]})
+    return jsonify({"success": True,
+                    "drinks": [drink.long() for drink in drinks]})
+
 
 @app.route('/drinks', methods=['POST'])
 @requires_auth("post:drinks")
@@ -46,26 +49,24 @@ def add_drink(token):
     new_recipe = json.dumps([ingredient for ingredient in new_recipe])
 
     try:
-        new_drink=Drink(title=new_title,recipe=new_recipe)
+        new_drink = Drink(title=new_title, recipe=new_recipe)
         new_drink.insert()
-
-    except:
+    except Exception:
         abort(422)
 
-    return jsonify({"success":True, "drinks":new_drink.long()})
+    return jsonify({"success": True, "drinks": new_drink.long()})
+
 
 @app.route('/drinks/<id>', methods=['PATCH'])
 @requires_auth("patch:drinks")
-def edit_drink(token,id):
+def edit_drink(token, id):
     drink_id = id
     body = request.get_json()
 
     new_title = body.get('title', None)
     new_recipe = body.get('recipe', None)
-    print(new_title)
-    print(new_recipe)
 
-    drink = Drink.query.filter(Drink.id==drink_id).one_or_none()
+    drink = Drink.query.filter(Drink.id == drink_id).one_or_none()
     if drink is None:
         abort(404)
 
@@ -73,32 +74,34 @@ def edit_drink(token,id):
         if new_title is not None:
             drink.title = new_title
             drink.update()
-        
+
         if new_recipe is not None:
             new_recipe = json.dumps([ingredient for ingredient in new_recipe])
             drink.recipe = new_recipe
             drink.update()
 
-    except:
+    except Exception:
         abort(422)
 
-    return jsonify({"success":True, "drinks":drink.long()})
+    return jsonify({"success": True, "drinks": [drink.long()]})
+
 
 @app.route('/drinks/<id>', methods=['DELETE'])
 @requires_auth("delete:drinks")
-def delete_drink(token,id):
+def delete_drink(token, id):
     drink_id = id
 
-    drink = Drink.query.filter(Drink.id==drink_id).one_or_none()
+    drink = Drink.query.filter(Drink.id == drink_id).one_or_none()
     if drink is None:
         abort(404)
 
     try:
         drink.delete()
-    except:
+    except Exception:
         abort(422)
 
-    return jsonify({"success":True, "delete":drink_id})
+    return jsonify({"success": True, "delete": drink_id})
+
 
 # Error Handling
 @app.errorhandler(422)
@@ -109,6 +112,7 @@ def unprocessable(error):
         "message": "unprocessable"
     }), 422
 
+
 @app.errorhandler(404)
 def unprocessable(error):
     return jsonify({
@@ -116,6 +120,7 @@ def unprocessable(error):
         "error": 404,
         "message": "resource not found"
     }), 404
+
 
 @app.errorhandler(AuthError)
 def unprocessable(AuthError):
