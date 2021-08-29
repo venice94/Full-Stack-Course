8e75540970cb164977dd1c7f38a2110311fa23b6
venice94 <venice9394@gmail.com>
2021-08-30 02:15:24 +0800

    heroku deployment

--- projects/capstone/starter/config.py
+++ projects/capstone/starter/config.py
@@ -10,5 +10,5 @@ DEBUG = True
 
 
 # TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI ='postgresql://sh_ubuntu:93water94@localhost:5432/wallet'
+SQLALCHEMY_DATABASE_URI ='postgresql-flexible-05314'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
